511d2d077fe3a4cb3daf87c31169ea0b0291717c
Jorge Gonzalez <jorgegoca95@gmail.com>
2026-07-08 03:04:04 -0600

    feat(laboratory): initialize project structure with HTML skeleton and boilerplate API connection logic

---
+++ LAB#2_BuscadorComparador/js/main.js
@@ -0,0 +1,12 @@
+// async function probarConexion() {
+//     const respuesta = await fetch("https://worldcup26.ir/get/teams?name_en=Argentina", {
+//         headers: {
+//             "Authorization": "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjZhNGRmNDQ4YjMwMTVlNWJmMzFjMzA2MiIsImlhdCI6MTc4MzQ5MzcwNCwiZXhwIjoxNzkwNzUxMzA0fQ.M9I1AE9RXvXKMcgxRJCucn856WvWwAdcDQ1SzBz6BLM"
+//         }
+//     });
+//     console.log(respuesta.status);
+
+//     const datos = await respuesta.json();
+//     console.log(datos);
+// }
+// probarConexion();
